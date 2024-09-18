3a38cf6f193471e0be976f2f3db1784e3d2eb8e4
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-09-18 15:59:03 +0700

    Jobsheet Minggu 4 - Praktikum 1 | Variabel dan Konstanta

---
+++ minggu4/variabelkonstanta.php
@@ -0,0 +1,28 @@
+<?php
+// $angka1 = 10;
+// $angka2 = 5;
+// $hasil = $angka1 + $angka2;
+// echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+// $benar = true;
+// $salah = false;
+// echo "Variabel benar: $benar, Variabel salah: $salah";
+// define("NAMA_SITUS","Websiteku.com");
+// define("TAHUN_PENDIRIAN", 2023);
+
+// echo "Selamat datang di" . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . "."
+// $a = 10;
+// $b = 5;
+// $c = $a + 5;
+// $d = $b + (10*5);
+// $e = $d - $c;
+
+// echo "Variabel a: {$a} <br>";
+// echo "Variabel b: {$b} <br>";
+// echo "Variabel c: {$c} <br>";
+// echo "Variabel d: {$d} <br>";
+// echo "Variabel e: {$e} <br>";
+
+// var_dump($e);
+
+
+?>
