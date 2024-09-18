6274b2d41f31c059690eafc6fd77dc10d6478493
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-09-18 16:24:21 +0700

    Jobsheet Minggu 4 - Praktikum 2 | Penggunaan Tipe Data

--- minggu4/variabelkonstanta.php
+++ minggu4/variabelkonstanta.php
@@ -9,20 +9,5 @@
 // define("NAMA_SITUS","Websiteku.com");
 // define("TAHUN_PENDIRIAN", 2023);
 
-// echo "Selamat datang di" . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . "."
-// $a = 10;
-// $b = 5;
-// $c = $a + 5;
-// $d = $b + (10*5);
-// $e = $d - $c;
-
-// echo "Variabel a: {$a} <br>";
-// echo "Variabel b: {$b} <br>";
-// echo "Variabel c: {$c} <br>";
-// echo "Variabel d: {$d} <br>";
-// echo "Variabel e: {$e} <br>";
-
-// var_dump($e);
-
 
 ?>
