f861710a2c7af77670bb2513af30777765b19216
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-10-11 08:35:38 +0700

    Jobsheet Minggu 7 - Praktikum 2 | Fungsi Empty

---
+++ minggu7/empty.php
@@ -0,0 +1,9 @@
+<?php
+$myArray = array();
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "Variabel sudah terdefinisi dan tidak kosong";
+}
+?>
