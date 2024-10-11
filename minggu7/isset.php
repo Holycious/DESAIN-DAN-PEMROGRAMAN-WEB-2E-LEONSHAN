0c69baa0ae78544e7b53838ede985589b6577890
Leon Shan <144356027+Holycious@users.noreply.github.com>
2024-10-11 08:18:57 +0700

    Jobsheet Minggu 7 - Praktikum 1 | Fungsi Isset

---
+++ minggu7/isset.php
@@ -0,0 +1,11 @@
+<?php
+
+$umur;
+$data = array ("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: <br>" . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan";
+}
+
+?>
